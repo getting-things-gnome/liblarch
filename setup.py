86f8a86efe90f2323c1319a41926d5c7452a7eb5
Izidor Matušov <izidor.matusov@gmail.com>
2012-02-12 23:17:08 +0100

    First quick-and-dirty script to build deb packages. You might want to change maintainer to sign deb packages with your GPG

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 
 from distutils.core import setup
 
-for package in ['liblarch', 'liblarch_gtk']:
+standalone_packages = ['liblarch', 'liblarch_gtk']
+
+for package in standalone_packages:
     setup(
       version = '0.1',
       url = 'https://live.gnome.org/liblarch',
-      author = 'Lionel Dricot & Izidor Matušov',
+      author = 'Liblarch team',
       author_email = 'gtg-contributors@lists.launchpad.net',
+      maintainer = 'Izidor Matušov',
+      maintainer_email = 'izidor.matusov@gmail.com',
       description = 'Liblarch is a python library built to easily handle data structure such are lists, trees and acyclic graphs (tree where nodes can have multiple parents)',
       license = 'LGPLv3',
 
